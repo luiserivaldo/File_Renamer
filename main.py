368e602abbe8e4f2d72bffbc569e3e8926c9a862
luise <luiserivaldo99@gmail.com>
2022-03-04 21:12:35 +0700

    add comments for code readability add requirement to press 'enter' to quit the tool

--- main.py
+++ main.py
@@ -12,19 +12,22 @@ def main():
     directory = input(str("Enter the folder name/path where files need to be renamed: "))
     if directory == "":
         print("No custom path. Directory set to 'images'. \n")
-        directory = "images/"
         # set target directory to "images" folder within project folder
+        directory = "images/"
     else:
         directory += '/'
+
+    # user input for the first searched
     old_word = input(str("Enter the word that needs to be replaced: "))
+    # user input for the word that replaces the first input
     new_word = input(str("Enter the new word to replace the original: "))
 
     try:
         file_renamer(directory, old_word, new_word)
         revert_name(directory, new_word, old_word)
-        # repeat_tool()
     except FileNotFoundError:
         print("No file or path detected. Current directory is ", directory, ".")
+    repeat_tool()
 
 
 def file_renamer(directory, old_word, new_word):
@@ -33,7 +36,8 @@ def file_renamer(directory, old_word, new_word):
         if old_word in file:
             os.rename(os.path.join(directory + file),
                       os.path.join(directory + file.replace(old_word, new_word)))
-            count = count + 1  # counts how many times function has been executed
+            # counts how many times function has been executed
+            count = count + 1
     print(count, "files have been renamed. \n")
     print("Please check your files if they have been properly renamed. \n")
 
@@ -55,15 +59,16 @@ def revert_name(directory, new_word, old_word):
 
 def repeat_tool():
     repeat_input = input(str("Would you like to rename more files? [Y/N] "))
-    if repeat_input.lower() == "y" or "yes":
+    if repeat_input.lower() == "y" or repeat_input == "yes":
         print("Restarting tool... \n")
-        print(repeat_input)
         main()
-    elif repeat_input.lower == "n" or "no":
+    elif repeat_input.lower == "n" or repeat_input == "no":
         print(repeat_input)
         print("Thank you for using the tool!")
+        print(input("Press 'enter' to close the program..."))
+        sys.exit()
     else:
-        print("Invalid input. Closing tool.")
+        print(input("Invalid input. Press 'enter' to close the program..."))
         sys.exit()
 
 
